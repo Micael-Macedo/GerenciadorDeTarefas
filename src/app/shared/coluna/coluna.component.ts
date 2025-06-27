60abc8d9fa462a20fd4a6d917bb99d88279675ac
Micael Macedo <micaelm2009@hotmail.com>
2025-06-26 23:44:00 -0300

    feat: add reconhecimento cor coluna e display

--- src/app/shared/coluna/coluna.component.ts
+++ src/app/shared/coluna/coluna.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, Input } from '@angular/core';
 import { IColuna } from '../../../interfaces/coluna.interface';
 import { ColunaService } from '../../../services/coluna.service';
 import { TarefaComponent } from "../tarefa/tarefa.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-coluna',
-  imports: [TarefaComponent],
+  imports: [TarefaComponent, CommonModule],
   templateUrl: './coluna.component.html',
   styleUrl: './coluna.component.scss'
 })
