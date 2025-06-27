d943e8d2ae4ce1536ee7392a7d829727ce779090
Micael Macedo <micaelm2009@hotmail.com>
2025-06-26 23:43:26 -0300

    feat: add exibicao dados tarefa e metodos

--- src/app/shared/tarefa/tarefa.component.ts
+++ src/app/shared/tarefa/tarefa.component.ts
@@ -1,12 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ITarefa } from '../../../interfaces/tarefa.interface';
+import { TarefaService } from '../../../services/tarefa.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tarefa',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tarefa.component.html',
   styleUrl: './tarefa.component.scss'
 })
 export class TarefaComponent {
   @Input() tarefa!: ITarefa;
+  readonly tarefaService = inject(TarefaService);
+
+  editTarefa(tarefa: ITarefa): void {
+    console.log('Editando tarefa:', tarefa);
+  }
+
+  deleteTarefa(id: number): void {
+    this.tarefaService.deleteTarefa(id).subscribe({
+      next: () => {
+        console.log('Tarefa deletada com sucesso');
+      },
+      error: (err) => {
+        console.error('Erro ao deletar tarefa:', err);
+      }
+    });
+  }
 }
